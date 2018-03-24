ba9958185a804bb562f5be66f32bd082fd8a1811
TimmyTimsen <tim@hable-net.com>
2018-03-24 14:57:45 +0000

    Username

--- js/index.js
+++ js/index.js
@@ -1,20 +1,45 @@
 let speaking = false;
+let selected = null;
+let isUsernameSet = false;
 
 window.addEventListener("load", function () {
-    setButtonSize();
+    initButton();
+    location.href = "#popup1";
+
+    document.getElementById("usernameOkButton").addEventListener("click", function () {
+        let textFieldValue = document.getElementById("usernameTF").value;
+        if (textFieldValue !== "" && textFieldValue !== null && !isUsernameSet) {
+            document.getElementById("userInfoDiv").appendChild(document.createTextNode("Username: " + textFieldValue));
+            location.href = "#";
+            isUsernameSet = true;
+        }
+    });
+
+    addGroup("Group0");
+    addGroup("Group1");
+    addGroup("Group2");
+    addGroup("Group3");
+    addGroup("Group4");
+
+    addParticipant("Raul");
+    addParticipant("Steffen");
+    addParticipant("Philip");
+    addParticipant("Michael");
 });
 
 window.addEventListener("resize", function () {
-    setButtonSize();
+    initButton();
 });
 
-function setButtonSize() {
+function initButton() {
     let pttButton = document.getElementById("PTTButton");
     let surroundingDiv = document.getElementById("pttDiv");
     let size = (surroundingDiv.offsetWidth * 0.8) + "px";
     pttButton.style.height = size;
     pttButton.style.width = size;
     pttButton.style.fontSize = (surroundingDiv.offsetWidth * 0.3) + "px";
+    pttButton.style.backgroundColor = "#fff";
+    pttButton.style.color = "#dc3545";
 
     pttButton.addEventListener("mousedown", function () {
         this.style.backgroundColor = "#dc3545";
@@ -27,8 +52,8 @@ function setButtonSize() {
     });
 }
 
-document.addEventListener("keydown", function(event) {
-    if(event.which === 32 && !speaking) {
+document.addEventListener("keydown", function (event) {
+    if (event.which === 32 && !speaking) {
         speaking = true;
         document.getElementById("PTTButton").dispatchEvent(new Event("mousedown"));
         document.getElementById("PTTButton").dispatchEvent(new Event("mouseover"));
@@ -36,9 +61,32 @@ document.addEventListener("keydown", function(event) {
 });
 
 document.addEventListener("keyup", function (event) {
-   if(event.which === 32) {
-       speaking = false;
-       document.getElementById("PTTButton").dispatchEvent(new Event("mouseup"));
-       document.getElementById("PTTButton").dispatchEvent(new Event("mouseout"));
-   }
+    if (event.which === 32) {
+        speaking = false;
+        document.getElementById("PTTButton").dispatchEvent(new Event("mouseup"));
+        document.getElementById("PTTButton").dispatchEvent(new Event("mouseout"));
+    }
 });
+
+function addGroup(group) {
+    addToList(document.getElementById("groupList"), group, function () {
+        if (selected !== null) {
+            selected.style.backgroundColor = "#fff";
+        }
+        this.style.backgroundColor = "rgba(255, 0, 0, 0.3)";
+        selected = this;
+    });
+}
+
+function addParticipant(participant) {
+    addToList(document.getElementById("participantList"), participant, function () {
+    });
+}
+
+function addToList(list, item, func) {
+    let listItem = document.createElement("li");
+    listItem.appendChild(document.createTextNode(item));
+    listItem.addEventListener("click", func);
+    listItem.className = "list-group-item";
+    list.appendChild(listItem);
+}
